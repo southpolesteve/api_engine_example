28e147fcd2cbf5e42a2a45576ccf199c4868acab
Steve Faulkner <southpolesteve@gmail.com>
2013-05-07 00:29:42 -0500

    Add whitelist for comment and post models

--- config/initializers/api_engine.rb
+++ config/initializers/api_engine.rb
@@ -1,9 +1,5 @@
 ApiEngine.configure do |config|
 
-  # Whitelist is a way to restrict API access to only a subset of models.
-  # by default this is set to 'false' and all models are exposed via the
-  # default API
-  #
-  # config.whitelist = [:post, :comment, :user]
+  config.whitelist = [:post, :comment]
 
 end
