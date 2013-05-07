feac33b37e659e76061251aedc0543a1190358d3
Steve Faulkner <southpolesteve@gmail.com>
2013-05-07 00:28:22 -0500

    Add api engine config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ApiEngineExample::Application.routes.draw do
+  resources :comments
+
+
+  resources :posts
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
